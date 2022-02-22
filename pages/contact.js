1c5eb050c4612787283175563321d03ea81a4b81
Fariz Keynes <muhafariz@gmail.com>
2022-02-22 10:31:45 +0700

    Contact, trigger deploy build vercel

--- pages/contact.js
+++ pages/contact.js
@@ -3,16 +3,11 @@ import Layout from "components/common/Layout";
 export default function Contact() {
   return (
     <Layout meta={{ title: "Contact" }}>
+      <h1>Contact</h1>
       <p className="lead">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi,
         similique! Lorem ipsum dolor sit amet.
       </p>
-      <h1 className="">
-        Hey There, I am{" "}
-        <span className="underline decoration-sky-500">Fariz</span>, a Software
-        Engineer specialized in{" "}
-        <span className="underline decoration-rose-500">Web-Based Apps</span>
-      </h1>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat ut eius
         qui praesentium, reprehenderit alias dignissimos molestiae maxime ab,
@@ -21,6 +16,7 @@ export default function Contact() {
         explicabo cupiditate? Magnam dolorum praesentium fugit tenetur atque. Ab
         repellat quas earum.
       </p>
+      <hr />
       <p>
         In magnam laboriosam dolor enim suscipit, expedita laborum hic numquam
         accusantium necessitatibus nulla molestiae. Placeat distinctio mollitia
@@ -28,14 +24,6 @@ export default function Contact() {
         maxime officia non reiciendis consectetur ipsam pariatur. Soluta
         blanditiis animi enim quia reprehenderit consequatur nobis magni amet
         temporibus quibusdam assumenda fuga sequi, accusantium praesentium.
-      </p>
-      <p>
-        Optio consequatur quibusdam corporis reiciendis hic dolorem cum natus
-        laborum dolor modi quo fugiat iusto, maiores quia dicta accusamus
-        reprehenderit qui est nisi illum! Repellendus sed minus ut modi vel quo
-        iusto aliquid in repudiandae asperiores expedita ipsum aperiam maiores
-        aspernatur odio sint, dolorum, quos neque? Nobis voluptatem
-        necessitatibus porro?
       </p>
     </Layout>
   );
